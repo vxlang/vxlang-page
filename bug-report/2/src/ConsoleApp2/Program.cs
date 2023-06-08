b9d2418dfe774e7fb4f2487d5e388ac8135f11ac
0a777h <0a777h@gmail.com>
2023-06-08 20:58:12 +0900

    #2, bug-report

---
+++ bug-report/2/src/ConsoleApp2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
