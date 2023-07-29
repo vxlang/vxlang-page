52640fb0294dceaf5492f9066d9aa6ea630f2b3f
0a777h <0a777h@gmail.com>
2023-07-29 18:50:16 +0900

    update SDK

--- vxlang/sdk/include/vxlib.h
+++ vxlang/sdk/include/vxlib.h
@@ -13,43 +13,28 @@ void __stdcall VxVirtualizationEnd();
 void __stdcall VxObfuscationBegin();
 void __stdcall VxObfuscationEnd();
 
+void __stdcall VxCodeFlatteningBegin();
+void __stdcall VxCodeFlatteningEnd();
+
 #define VL_OBFUSCATION_BEGIN         VxObfuscationBegin()
 #define VL_OBFUSCATION_END           VxObfuscationEnd()
 
+#define VL_CODE_FLATTENING_BEGIN     VxCodeFlatteningBegin()
+#define VL_CODE_FLATTENING_END       VxCodeFlatteningEnd()
+
 #define VL_VIRTUALIZATION_BEGIN      VxVirtualizationBegin()
 #define VL_VIRTUALIZATION_END        VxVirtualizationEnd()
+
 #else
 #define VL_OBFUSCATION_BEGIN
 #define VL_OBFUSCATION_END
+
+#define VL_CODE_FLATTENING_BEGIN 
+#define VL_CODE_FLATTENING_END 
 
 #define VL_VIRTUALIZATION_BEGIN            
 #define VL_VIRTUALIZATION_END        
 #endif
 
-
-
-/*
- *
- */
-
-#ifdef VXLANG_ADDON_MODULE
-
-#pragma comment(linker, "/ALIGN:0x10000")
-
-enum _tag_PACKER_STATUS {
-    VXLANG_INITIALIZE = 0,
-    VXLANG_LOAD_ADDON_EVENT = 0xA0000000,
-
-    VXLANG_START_EVENT,
-    VXLANG_TERMINATE_EVENT,
-
-    VXLANG_ERROR_IMPORT_TABLE = 0xE0000000,
-    VXLANG_ERROR_ALLOCATE_VIRTUAL_IMAGE,
-    VXLANG_ERROR_RELOCATE_IMAGE,
-    VXLANG_ERROR_INSTALL_FAIL,
-
-    VXLANG_DETECTED_DEBUG = 0xD0000000,
-    VXLANG_DETECTED_PATCH,
-};
 #endif
 
