9ee2b8fe63a5f54ebda443ea31f402430b93eafe
0a777h <0a777h@gmail.com>
2026-02-25 21:03:01 +0900

    update

--- src/SDK/deploy/include/vxlib.h
+++ src/SDK/deploy/include/vxlib.h
@@ -1,5 +1,16 @@
 #pragma once
 
+/*
+ *
+ */
+
+#define VXLANG_LINK_EVENT_STR_A                    0xA0000009
+#define VXLANG_LINK_EVENT_STR_W                    0xA000000A
+#define VXLANG_LINK_EVENT_FREE                     0xA000000B
+#define VXLANG_LINK_CALLBACK                       0xA000000C
+
+ //
+
 #ifdef USE_VL_MACRO
 extern "C"
 void VxVirtualizationBegin();
@@ -17,14 +28,28 @@ extern "C"
 void VxObfuscationEnd();
 
 extern "C"
+void VxLiteObfuscationBegin();
+
+extern "C"
+void VxLiteObfuscationEnd();
+
+extern "C"
 void VxCodeFlatteningBegin();
 extern "C"
 void VxCodeFlatteningEnd();
 
 //
 
+extern "C"
+void* VxLinkEvent(unsigned int id, void* buffer, size_t* sizeOfBuffer);
+
+//
+
 #define VL_OBFUSCATION_BEGIN               VxObfuscationBegin()
 #define VL_OBFUSCATION_END                 VxObfuscationEnd()
+
+#define VL_LITE_OBFUSCATION_BEGIN          VxLiteObfuscationBegin()
+#define VL_LITE_OBFUSCATION_END            VxLiteObfuscationEnd()
 
 #define VL_CODE_FLATTENING_BEGIN           VxCodeFlatteningBegin()
 #define VL_CODE_FLATTENING_END             VxCodeFlatteningEnd()
@@ -34,9 +59,16 @@ void VxCodeFlatteningEnd();
 
 #define VL_DUAL_MODE_BEGIN                 VxDualModeBegin()
 #define VL_DUAL_MODE_END                   VxDualModeEnd()
+
+#define VL_ALLOC_STR(str)                  (const char*)VxLinkEvent(VXLANG_LINK_EVENT_STR_A, (void*)str, nullptr)
+#define VL_ALLOC_WSTR(str)                 (const wchar_t*)VxLinkEvent(VXLANG_LINK_EVENT_STR_W, (void*)str, nullptr)
+#define VL_FREE(value)                     VxLinkEvent(VXLANG_LINK_EVENT_FREE, (void*)value, nullptr);
 #else
 #define VL_OBFUSCATION_BEGIN            
 #define VL_OBFUSCATION_END              
+
+#define VL_LITE_OBFUSCATION_BEGIN          
+#define VL_LITE_OBFUSCATION_END            
 
 #define VL_CODE_FLATTENING_BEGIN        
 #define VL_CODE_FLATTENING_END          
@@ -46,7 +78,9 @@ void VxCodeFlatteningEnd();
 
 #define VL_DUAL_MODE_BEGIN
 #define VL_DUAL_MODE_END
+
+#define VL_ALLOC_STR(str)
+#define VL_ALLOC_WSTR(str)
+#define VL_FREE(value)
 #endif
-
-
 
